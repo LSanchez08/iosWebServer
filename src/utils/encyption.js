17ee4d0b54c89d9808e70a714fb8d59ed83dbd3a
LSanchez08 <luis.alberto.sanchez.rios@live.com.mx>
2022-05-12 18:01:23 -0500

    Bcrypt

--- src/utils/encyption.js
+++ src/utils/encyption.js
@@ -1,9 +1,12 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   encryptPassword: async (password) => {
     const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
+    console.log({salt})
+    const newPass = await bcrypt.hash(password, salt);
+    // console.log({newPass})
+    return newPass;
   },
   matchPassword: async (password, dbPassword) => {
     return await bcrypt.compare(password, dbPassword);
